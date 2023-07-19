fe10d3c1e89830ff493645c88872818c353583a0
Prajwal Bobe <127470337+prajwalbobe302@users.noreply.github.com>
2023-07-20 01:08:35 +0530

    Add files via upload

---
+++ BookRepository.java
@@ -0,0 +1,9 @@
+package com.bookStore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bookStore.entity.Book;
+
+public interface BookRepository extends JpaRepository<Book,Integer>  {
+
+}
